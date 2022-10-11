fff7f20eae4a70b61d11686a21ff5db344337085
Long Hoang <longhoang12212@gmai.com>
2022-10-11 10:28:49 +0700

    Add BaiTap_3 file

---
+++ BaiTap_3.js
@@ -0,0 +1,32 @@
+
+function Add(){
+    unNum1 = document.getElementById("num1").value;
+    unNum2 = document.getElementById("num2").value;
+    var num1 = parseInt(unNum1);
+    var num2 = parseInt(unNum2);
+    document.write(num1 + num2);
+}
+
+function Sub(){
+    unNum1 = document.getElementById("num1").value;
+    unNum2 = document.getElementById("num2").value;
+    var num1 = parseInt(unNum1);
+    var num2 = parseInt(unNum2);
+    document.write(num1 - num2);
+}
+
+function Multi(){
+    unNum1 = document.getElementById("num1").value;
+    unNum2 = document.getElementById("num2").value;
+    var num1 = parseInt(unNum1);
+    var num2 = parseInt(unNum2);
+    document.write(num1 * num2);
+}
+
+function Div(){
+    unNum1 = document.getElementById("num1").value;
+    unNum2 = document.getElementById("num2").value;
+    var num1 = parseInt(unNum1);
+    var num2 = parseInt(unNum2);
+    document.write(num1 / num2);
+}
